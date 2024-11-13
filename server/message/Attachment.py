4b1c58ac8fae6c4694ee62b71ffe83658723bf71
Micah Baumann <micah.ad.mail@gmail.com>
2024-11-13 15:46:39 -0800

    Update

---
+++ server/message/Attachment.py
@@ -0,0 +1,4 @@
+
+class Attachment:
+    def __init__(self):
+        pass
--- server/message/__init__.py
+++ server/message/__init__.py
@@ -1,5 +1,7 @@
-__all__ = ["message_parse", "MessageLine", "NuMailMessage"]
+__all__ = ["message_parse", "MessageLine", "NuMailMessage", "server_parser", "Attachment"]
 
 from . import message_parse
 from . import MessageLine
 from . import NuMailMessage
+from . import server_parser
+from . import Attachment
