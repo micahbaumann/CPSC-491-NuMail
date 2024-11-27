daffb7dda22d2b4129e43b1d0a2b90b3abc8a3e9
Micah Baumann <micah.ad.mail@gmail.com>
2024-11-27 01:44:55 -0800

    Update

--- server/message/modules/data.py
+++ server/message/modules/data.py
@@ -10,17 +10,39 @@ async def mod_data(reader, writer, message, local_stack, state, loop):
         state["data"] = ""
         return
     else:
-        if local_stack[-1] == '.\r\n':
-            message.set_payload(state["data"])
-            loop.returnLoop()
-            writer.write(MessageLine(f"250 2.0.0 Message accepted for delivery", message).bytes())
-            await writer.drain()
-            return
+        data_str = local_stack[-1]
+        local_loop = False
+        if local_stack[-1].find("\r\n"):
+            data_str = local_stack[-1].split("\r\n")
+            local_loop = True
+        if local_loop:
+            for data_line in data_str:
+                if data_line == '.\r\n' or data_line == '.':
+                    message.set_payload(state["data"])
+                    loop.returnLoop()
+                    writer.write(MessageLine(f"250 2.0.0 Message accepted for delivery", message).bytes())
+                    await writer.drain()
+                    return
+                else:
+                    line = data_line
+                    if len(line) > 0 and line[0] == '.':
+                        line = line[1:]
+                    
+                    state["data"] += line
         else:
-            line = local_stack[-1]
-            if len(line) > 0 and line[0] == '.':
-                line = line[1:]
-            
-            state["data"] += line
+            if data_str == '.\r\n':
+                print("p1")
+                message.set_payload(state["data"])
+                loop.returnLoop()
+                writer.write(MessageLine(f"250 2.0.0 Message accepted for delivery", message).bytes())
+                await writer.drain()
+                return
+            else:
+                print("p2")
+                line = data_str
+                if len(line) > 0 and line[0] == '.':
+                    line = line[1:]
+                
+                state["data"] += line
     
     loop.continueLoop()
