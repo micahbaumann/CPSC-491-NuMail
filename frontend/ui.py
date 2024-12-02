39528e594f4c61b95d7fc5d2777a877354b1a953
Micah Baumann <micah.ad.mail@gmail.com>
2024-12-02 00:09:59 -0800

    Update

--- frontend/ui.py
+++ frontend/ui.py
@@ -1,14 +1,28 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, session, redirect, url_for, request, jsonify
 
 app = Flask(__name__)
 
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+
 @app.route("/")
-def hello_world():
+def index():
     return "<p>Hello, World!</p>"
 
-@app.route("/login")
+@app.route("/login", methods=['GET', 'POST'])
 def login():
+    if request.method == 'POST':
+        session['username'] = request.form['email']
+        return jsonify({
+            "status": "success"
+        })
+    if 'username' in session:
+        return redirect(url_for('index'))
     return render_template('login.html')
+
+@app.route('/logout')
+def logout():
+    session.pop('username', None)
+    return redirect(url_for('login'))
 
 if __name__ == '__main__':
     app.run()
