13f879c5ff706651e066bd79aadb82b89fce39e3
Micah Baumann <micah.ad.mail@gmail.com>
2024-12-12 17:14:11 -0800

    Update

--- server/message/message_parse.py
+++ server/message/message_parse.py
@@ -547,8 +547,11 @@ writer: An asyncio writer object
 message_stack: The message stack
 """
 async def email_parse(reader, writer, message_stack):
-    writer.write(b"email\r\n")
+    # writer.write(b"email\r\n")
+    # await writer.drain()
+    # msg = await reader.read(1024)
+    # writer.write(f"{msg.strip()}\r\n".encode("ascii"))
+    # await writer.drain()
+    writer.write(MessageLine(f"500 6.0.0 SMTP not supported", message_stack).bytes())
     await writer.drain()
-    msg = await reader.read(1024)
-    writer.write(f"{msg.strip()}\r\n".encode("ascii"))
-    await writer.drain()
+    return "exit"
